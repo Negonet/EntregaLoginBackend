efd9dbf88cfc1f1d6181b861a3d24754a6b9a7ad
unknown <l.vazquezrun@gmail.com>
2023-12-07 22:28:32 -0300

    Segunda PreEntrega de PF

--- source/routes/views.router.js
+++ source/routes/views.router.js
@@ -7,7 +7,8 @@ router.get('/home', async(req,res)=> {
     try {
         const prod = await products.getProducts();
         console.log(prod)
-        res.render('home',{prod});
+        res.render('home', {
+            prod: prod.map(prod => prod.toJSON())});
 
     } catch (err) {
         res.status(500).json({message:err.message});
